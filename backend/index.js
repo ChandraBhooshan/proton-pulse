5c6b51980cef5887632cfdd33694c16acd5e13f5
Chandra Bhooshan <chandrabhooshanjnvsn@gmail.com>
2026-07-15 00:14:30 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const AuthRouter = require('./Routes/AuthRouter');
-const ProductRouter = require('./Routes/ProductRouter');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require("./Routes/AuthRouter");
+const ProductRouter = require("./Routes/ProductRouter");
 
-require('dotenv').config();
-require('./Models/db');
+require("dotenv").config();
+require("./Models/db");
 const PORT = process.env.PORT || 8080;
 
-app.get('/ping', (req, res) => {
-    res.send('PONG');
+app.get("/ping", (req, res) => {
+  res.send("PONG");
 });
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/signup-login/auth', AuthRouter);
-app.use('/signup-login/products', ProductRouter);
+// app.use('/signup-login/auth', AuthRouter);
+app.use("/signup-login/auth", (req, res) => {
+  res.send("PONG_AUTH");
+});
 
+app.use("/signup-login/products", ProductRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-})
+  console.log(`Server is running on ${PORT}`);
+});
