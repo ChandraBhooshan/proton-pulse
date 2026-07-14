a622391f5958f81c97204d517773efa8ad011b3f
Chandra Bhooshan <chandrabhooshanjnvsn@gmail.com>
2026-07-15 00:25:18 +0530

    commit

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -6,10 +6,10 @@ const {
 
 const router = require("express").Router();
 
-// router.post('/login', loginValidation, login);
-router.get("/login", (req, res) => {
-  res.send("PONG_AUTH_LOGIN");
-});
+router.post('/login', loginValidation, login);
+// router.get("/login", (req, res) => {
+//   res.send("PONG_AUTH_LOGIN");
+// });
 
 router.post("/signup", signupValidation, signup);
 
